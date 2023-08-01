bce0f9d9eebd0889e5889eb82c220d1cdbc6e30a
Mandaline <mandy@wearecapicua.com>
2023-08-01 15:04:22 -0300

    add colors and navbar

--- components/alert.tsx
+++ components/alert.tsx
@@ -18,29 +18,14 @@ export default function Alert({ preview }: AlertProps) {
     >
       <Container>
         <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This is page is a preview.{" "}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                Click here
-              </a>{" "}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
+          This is page is a preview.{" "}
+          <a
+            href="/api/exit-preview"
+            className="underline hover:text-cyan duration-200 transition-colors"
+          >
+            Click here
+          </a>{" "}
+          to exit preview mode.
         </div>
       </Container>
     </div>
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
+import Navbar from '../components/navbar'
 
 type LayoutProps = {
   preview: boolean
@@ -12,7 +13,8 @@ export default function Layout({ preview, children }: LayoutProps) {
     <>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
+        {preview && <Alert preview={preview} /> }
+        <Navbar />
         <main>{children}</main>
       </div>
       <Footer />
