e741676c53eced7c6c5dbaa5bad264f5d75ba63f
Mandaline <mandy@wearecapicua.com>
2023-08-02 16:37:40 -0300

    faq section

--- slices/Faq/index.tsx
+++ slices/Faq/index.tsx
@@ -1,6 +1,7 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-
+import { Disclosure } from '@headlessui/react'
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
 /**
  * Props for `Faq`.
  */
@@ -10,12 +11,37 @@ export type FaqProps = SliceComponentProps<Content.FaqSlice>;
  * Component for "Faq" Slices.
  */
 const Faq = ({ slice }: FaqProps): JSX.Element => {
+  const question = slice.primary.question
+  const answer = slice.primary.answer
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for faq (variation: {slice.variation}) Slices
+      <div className="mx-auto max-w-6xl mx-6 py-6 border-t-2 border-gray-900/10 sm:py-32 lg:px-8 lg:py-6">
+        <Disclosure as="div" className="pt-6">
+          {({ open }) => (
+            <>
+              
+                <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                  <span className="text-base font-semibold leading-7">{question}</span>
+                  <span className="ml-6 flex h-7 items-center">
+                    {open ? (
+                      <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                    )}
+                  </span>
+                </Disclosure.Button>
+            
+              <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                <p className="text-base leading-7 text-gray-600">{answer}</p>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
     </section>
   );
 };
