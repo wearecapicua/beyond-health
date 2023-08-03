5d02014fde8bb0e2e110f36a1a2fa94029799ff4
Mandaline <mandy@wearecapicua.com>
2023-08-03 18:05:13 -0300

    finish large cta styles

--- slices/CtaSection/index.tsx
+++ slices/CtaSection/index.tsx
@@ -52,16 +52,16 @@ const CtaSection = ({ slice }: CtaSectionProps): JSX.Element => {
       <div className="mx-auto mb-10">
         <div className="cta-black-left-fade overflow-hidden sm:rounded-3xl lg:flex">
           <div className="mx-auto max-w-md pl-11 text-center flex flex-col justify-center cta-blue-black-fade lg:mx-0 lg:text-left">
-            <h2 className="font-sans text-white">
+            <h1 className="font-sans text-white w-[610px] z-10 leading-tight">
               {heading}
-            </h2>
-            <p className="mt-10 text-lg leading-8 text-gray-300">
+            </h1>
+            <p className="w-[515px] z-10 mt-6 text-xl leading-8 text-gray-300">
               {subheading}
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+            <div className="mt-6 flex items-center justify-center gap-x-6 lg:justify-start">
               <a
                 href="#"
-                className="rounded-full bg-accent-green px-16 py-3 text-xl font-semibold text-main-black shadow-sm hover:bg-accent-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-full bg-accent-green px-14 py-3 text-lg font-semibold text-main-black shadow-sm hover:bg-accent-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 {cta_button_text}
               </a>
