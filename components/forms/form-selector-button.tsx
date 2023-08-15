e3bc2e94e6092601e3f7287c68f9ebd412d818e0
Mandaline <mandy@wearecapicua.com>
2023-08-15 10:21:19 -0300

    clean up step 2 buttons

--- components/forms/form-selector-button.tsx
+++ components/forms/form-selector-button.tsx
@@ -4,7 +4,7 @@ type FormSelectorButtonProps = {
   selected: string;
   label: string;
   value: string;
-  id: string;
+  groupId: string;
   setSelected: (text: string) => void;
 };
 
@@ -13,38 +13,30 @@ export default function FormButton({
   setSelected,
   label,
   value,
-  id,
   groupId
 }: FormSelectorButtonProps) {
-  const handleChange = (value: string) => {
-    setSelected(value);
-  };
+
   const {
     register,
-    watch,
     formState: { errors },
-    control
   } = useFormContext();
 
-  const watchValue = watch(groupId)
-  console.log("error", errors)
   return (
     
     <div className="relative my-4 bg-white border-[1px] text-main-blue border-gray-400 rounded-full px-6 py-8 text-xl font-semibold leading-6">
     <input
       type="radio"
-      id={id}
+      id={value}
       value={value}
-      //checked={value}
       {...register(groupId, {
-        onChange: () => handleChange(value),
+        onChange: (e) => setSelected(e.target.value)
       })}
       className="w-full h-full opacity-0 absolute top-0"
     />
     <label
-      htmlFor={id}
+      htmlFor={value}
       className={`absolute inset-0 flex items-center justify-center cursor-pointer ${
-        watchValue === value
+        selected === value
           ? "bg-main-light-blue text-white"
           : "text-main-blue border-[1px] border-gray-400"
       } rounded-full`}
--- components/forms/steps/step-2.tsx
+++ components/forms/steps/step-2.tsx
@@ -5,10 +5,11 @@ import FormSelectorButton from "../form-selector-button";
 
 export default function StepTwo() {
   const [selected, setSelected] = useState("");
+
   const radioButtonOptions = [
-    { value: "male", label: "Male", field: "field-male" },
-    { value: "female", label: "Female", field: "field-female" },
-    { value: "trasgender", label: "Transgender / Non-binary", field: "field-trans" },
+    { value: "male", label: "Male" },
+    { value: "female", label: "Female" },
+    { value: "transgender", label: "Transgender / Non-binary" },
   ];
 
   return (
@@ -25,7 +26,6 @@ export default function StepTwo() {
               label={option.label}
               value={option.value}
               groupId="gender"
-              id={option.value}
               selected={selected}
               setSelected={setSelected}
             />
