74193929dd2aac4a3a3236f633d3bd44dc37437c
Mandaline <mandy@wearecapicua.com>
2023-09-14 13:58:19 -0300

    save

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,32 +1,8 @@
-import {
-  DynamoDB,
-  DynamoDBClientConfig,
-  ListTablesCommand,
-} from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { DynamoDBAdapter } from "@auth/dynamodb-adapter";
-
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+import jwt from "jsonwebtoken"
 import env from "lib/env";
-
-const config: DynamoDBClientConfig = {
-  credentials: {
-    accessKeyId: env.awsAccessKeyId,
-    secretAccessKey: env.awsSecretAccessKey,
-  },
-  region: env.awsRegion,
-};
-
-const client = DynamoDBDocument.from(new DynamoDB(config), {
-  marshallOptions: {
-    convertEmptyValues: true,
-    removeUndefinedValues: true,
-    convertClassInstanceToMap: true,
-  },
-});
-
-const tableName = `${env.vercelEnv}-beyond-health-users`;
 
 export const authOptions = {
   providers: [
@@ -35,58 +11,26 @@ export const authOptions = {
       clientSecret: env.googleClientSecret,
     }),
   ],
-  adapter: DynamoDBAdapter(client, {
-    tableName,
-  }) as any,
+  adapter: SupabaseAdapter({
+    url: env.nextPublicSupabaseUrl,
+    secret: env.supabaseServiceRoleKey,
+  }),
+    callbacks: {
+    async session({ session, user }: any) {
+      const signingSecret = process.env.SUPABASE_JWT_SECRET
+      if (signingSecret) {
+        const payload = {
+          aud: "authenticated",
+          exp: Math.floor(new Date(session.expires).getTime() / 1000),
+          sub: user.id,
+          email: user.email,
+          role: "authenticated",
+        }
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret)
+      }
+      return session
+    },
+  },
 };
-
+/* @ts-ignore */
 export default NextAuth(authOptions);
-
-
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import NextAuth from 'next-auth';
-
-// import {
-//   DynamoDB,
-//   DynamoDBClientConfig,
-//   ListTablesCommand,
-// } from "@aws-sdk/client-dynamodb";
-// import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-// import GoogleProvider from "next-auth/providers/google";
-// import { DynamoDBAdapter } from "@auth/dynamodb-adapter";
-// import { DefaultAdapter } from "next-auth/adapters";
-// import env from "lib/env";
-
-// const config: DynamoDBClientConfig = {
-//   credentials: {
-//     accessKeyId: env.awsAccessKeyId,
-//     secretAccessKey: env.awsSecretAccessKey,
-//   },
-//   region: env.awsRegion,
-// };
-
-// const client = DynamoDBDocument.from(new DynamoDB(config), {
-//   marshallOptions: {
-//     convertEmptyValues: true,
-//     removeUndefinedValues: true,
-//     convertClassInstanceToMap: true,
-//   },
-// });
-
-// const tableName = `${env.vercelEnv}-beyond-health-users`;
-
-// export const authOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: env.googleClientId,
-//       clientSecret: env.googleClientSecret,
-//     }),
-//   ],
-//   adapter: DynamoDBAdapter(client, {
-//     tableName,
-//   }) as DefaultAdapter,
-// };
-
-// export default (req: NextApiRequest, res: NextApiResponse) => {
-//   NextAuth(req, res, authOptions); // Pass both req and res objects to NextAuth
-// };
