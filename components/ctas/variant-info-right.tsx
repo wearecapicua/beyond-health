2a11a06f5bc0aa006f938ffbab49396f218d1006
Mandaline <mandy@wearecapicua.com>
2023-10-15 16:02:21 -0300

    m

--- components/ctas/variant-info-right.tsx
+++ components/ctas/variant-info-right.tsx
@@ -25,16 +25,16 @@ const InfoRightCta = ({ slice }: CtaSectionProps): JSX.Element => {
   } = slice.primary
 
   return (
-    <div className="mx-auto pt-10 pb-20">
-      <div className="lg:flex justify-between">
-        <div className="h-full overflow-hidden rounded-3xl h-[655px] max-w-[622px]">
+    <div id="variant-info-left" className="mx-auto pt-10 pb-20">
+      <div className="lg:flex justify-between px-5 sm:px-0">
+        <div className="h-full overflow-hidden rounded-3xl h-[655px] w-full lg:max-w-[622px]">
           <img
             src={image.url || undefined}
             alt="Beyond Health Logo"
             className="object-cover"
           />
         </div>
-        <div className="mx-auto w-[40%] flex flex-col lg:mx-0 lg:text-left">
+        <div className="mx-auto mt-14 lg:mt-0 lg:w-[40%] flex flex-col lg:mx-0 lg:text-left">
           <span className="font-bold text-main-blue text-lg">{introtext}</span>
           <h2 className="font-sans leading-tight mt-4">
             {heading}
@@ -78,7 +78,7 @@ const InfoRightCta = ({ slice }: CtaSectionProps): JSX.Element => {
           <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
             <a
               href="#"
-              className="rounded-full bg-main-light-blue px-14 py-3 text-lg font-semibold text-white shadow-sm hover:bg-main-light-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="w-full sm:w-auto text-center rounded-full bg-main-light-blue px-14 py-3 text-lg font-semibold text-white shadow-sm hover:bg-main-light-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               {cta_button_text}
             </a>
--- components/ctas/variant-large.tsx
+++ components/ctas/variant-large.tsx
@@ -18,19 +18,19 @@ const LargeCta = ({ slice }: CtaSectionProps): JSX.Element => {
   } = slice.primary
 
   return (
-    <div className="mx-auto pb-20">
-      <div className="cta-black-left-fade overflow-hidden sm:rounded-3xl lg:flex">
-        <div className="mx-auto max-w-md pl-11 text-center flex flex-col justify-center cta-blue-black-fade lg:mx-0 lg:text-left">
+    <div id="variant-lg" className="mx-auto pb-10 lg:pb-20">
+      <div className="cta-black-left-fade overflow-hidden rounded-3xl lg:flex mx-5 sm:mx-0">
+        <div className="pb-10 pt-14 px-6 lg:py-0 mx-auto lg:max-w-md lg:pl-11 text-center flex flex-col justify-center items-center lg:items-start cta-blue-black-fade lg:mx-0 lg:text-left">
           <h1 className="font-sans text-white w-[610px] z-10 leading-tight">
             {heading}
           </h1>
-          <p className="w-[515px] z-10 mt-6 text-xl leading-8 text-gray-300">
+          <p className="lg:w-[515px] z-10 mt-6 text-xl leading-8 text-gray-300">
             {subheading}
           </p>
-          <div className="mt-6 flex items-center justify-center gap-x-6 lg:justify-start">
+          <div className="w-full mt-8 lg:mt-6 flex items-center justify-center gap-x-6 lg:justify-start">
             <a
               href="#"
-              className="rounded-full bg-accent-green px-14 py-3 text-lg font-semibold text-main-black shadow-sm hover:bg-accent-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="w-full sm:w-auto rounded-full bg-accent-green px-14 py-3 text-lg font-semibold text-main-black shadow-sm hover:bg-accent-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               {cta_button_text}
             </a>
