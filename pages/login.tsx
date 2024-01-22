28a36933c51476bffab60e0c20e1abece7ade73c
Alexandrei <alexandreirdp@gmail.com>
2024-01-22 11:32:35 -0400

    2nd part

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,105 +1,122 @@
-import { useState, useEffect } from "react";
-import Head from "next/head";
-import Container from "components/container";
-import Layout from "components/layout";
-import FormHeader from "components/forms/form-header";
-import FormButton from "components/forms/form-button";
-import { signIn } from "next-auth/react";
-import useLoginStore from "store/login"
-import { useSession } from "next-auth/react";
-import { useRouter } from 'next/router';
-import { getProfileData } from "lib/api/supabase";
-import { useFormStore } from 'store/useFormStore';
+import { useEffect, useState } from 'react'
+
+import Container from 'components/container'
+import FormButton from 'components/forms/form-button'
+import FormHeader from 'components/forms/form-header'
+import Layout from 'components/layout'
+import { getProfileData } from 'lib/api/supabase'
+import { signIn, useSession } from 'next-auth/react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import useLoginStore from 'store/login'
+import { useFormStore } from 'store/useFormStore'
 
 type LoginProps = {
-  preview: boolean;
-  login: boolean;
-  fullPage: boolean;
+	preview: boolean
+	login: boolean
+	fullPage: boolean
 }
 
-export default function LoginPage({ preview }: LoginProps) {
-  const session = useSession();
-  const router = useRouter();
-  const userLoggedIn = session.status === "authenticated" && session.data?.user;
-  const [profileData, setProfileData] = useState();
-  const { updateFormStore } = useFormStore();
-  const { loginState, setLoginState } = useLoginStore()
-  const [loginDetails, setLoginDetails] = useState({
-    titleText: "Log In",
-    subtitleText: "Sign in to start your visit.",
-    buttonText: "Log in with Google",
-    loginSpan: "Don't have an account? ",
-    loginLink: "Sign Up",
-    image: "/images/login_image.jpg"
-  });
+const LoginPage = ({ preview }: LoginProps) => {
+	const session = useSession()
+	const router = useRouter()
+	const userLoggedIn = session.status === 'authenticated' && session.data?.user
+	const [profileData, setProfileData] = useState()
+	const { updateFormStore } = useFormStore()
+	const { loginState, setLoginState } = useLoginStore()
+	const [loginDetails, setLoginDetails] = useState({
+		titleText: 'Log In',
+		subtitleText: 'Sign in to start your visit.',
+		buttonText: 'Log in with Google',
+		buttonTextFacebook: 'Log in with Facebook',
+		loginSpan: "Don't have an account? ",
+		loginLink: 'Sign Up',
+		image: '/images/login_image.jpg'
+	})
 
-  useEffect(() => {
-    setLoginDetails({
-      titleText: loginState ? "Log In" : "Sign Up",
-      subtitleText: loginState ? "Sign in to start your visit." : "Create an account to start your visit.",
-      buttonText: loginState ? "Log in with Google" : "Sign up with Google",
-      loginSpan: loginState ? "Don't have an account? " : "Already have an account? ",
-      loginLink: loginState ? "Sign Up" : "Log In",
-      image: loginState ? "/images/login_image.jpg" : "/images/signup_image.jpg"
-    });
-  }, [loginState]);
+	useEffect(() => {
+		setLoginDetails({
+			titleText: loginState ? 'Log In' : 'Sign Up',
+			subtitleText: loginState ? 'Sign in to start your visit.' : 'Create an account to start your visit.',
+			buttonText: loginState ? 'Log in with Google' : 'Sign up with Google',
+			buttonTextFacebook: loginState ? 'Log in with Facebook' : 'Sign up with Facebook',
+			loginSpan: loginState ? "Don't have an account? " : 'Already have an account? ',
+			loginLink: loginState ? 'Sign Up' : 'Log In',
+			image: loginState ? '/images/login_image.jpg' : '/images/signup_image.jpg'
+		})
+	}, [loginState])
 
-  useEffect(() => {
-    const getUserProfileData = async () => {
-      const userProfileData = await getProfileData();
+	useEffect(() => {
+		const getUserProfileData = async () => {
+			const userProfileData = await getProfileData()
 
-      setProfileData(userProfileData);
-    }
-    
-    if (!profileData) {
-      getUserProfileData();
-    }
+			setProfileData(userProfileData)
+		}
 
-    if (userLoggedIn && profileData) {
-      updateFormStore(profileData);
-    }
+		if (!profileData) {
+			getUserProfileData()
+		}
 
-  }, [userLoggedIn, profileData]);
+		if (userLoggedIn && profileData) {
+			updateFormStore(profileData)
+		}
+	}, [userLoggedIn, profileData])
 
-  if (userLoggedIn) {
-    router.push('/form/step-1');
-    return null;
-  }
+	if (userLoggedIn) {
+		router.push('/form/step-1')
 
-  const handleToggle = () => {
-    setLoginState(!loginState);
-  };
+		return null
+	}
 
-  return (
-    <Layout preview={preview} fullPage >
-      <Head>
-        <title>Beyond Health</title>
-      </Head>
-      <Container>
-        <div className="flex h-[calc(100vh-4.5rem)] flex-1">
-          <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:w-1/2 lg:px-20 xl:px-24">
-            <div className="mx-auto w-full max-w-sm lg:w-96">
-              <FormHeader title={loginDetails.titleText} subtitle={loginDetails.subtitleText} />
-              <div className="mt-4 pb-20">
-                <FormButton type="submit" text={loginDetails.buttonText} icon="google" onClick={() => signIn("google")}/>
-                <div className="mt-10 text-center">
-                  <span className="bg-white px-6 text-gray-900">{loginDetails.loginSpan}
-                    <button className="underline" onClick={handleToggle}>{loginDetails.loginLink}</button>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="py-12 hidden w-0 flex-1 lg:block">
-            <img
-              className="h-full w-full rounded-3xl object-cover"
-              src={loginDetails.image}
-              alt="login image"
-            />
-          </div>
-        </div>
-      </Container>
-    </Layout>
-  );
+	const handleToggle = () => {
+		setLoginState(!loginState)
+	}
+
+	return (
+		<Layout preview={preview} fullPage>
+			<Head>
+				<title>Beyond Health</title>
+			</Head>
+			<Container>
+				<div className="flex h-[calc(100vh-4.5rem)] flex-1">
+					<div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:w-1/2 lg:flex-none lg:px-20 xl:px-24">
+						<div className="mx-auto w-full max-w-sm lg:w-96">
+							<FormHeader title={loginDetails.titleText} subtitle={loginDetails.subtitleText} />
+							<div className="mt-4 pb-20">
+								<FormButton
+									type="submit"
+									text={loginDetails.buttonTextFacebook}
+									icon="facebook"
+									onClick={() => signIn('facebook')}
+								/>
+								<FormButton
+									type="submit"
+									text={loginDetails.buttonText}
+									icon="google"
+									onClick={() => signIn('google')}
+								/>
+								<div className="mt-10 text-center">
+									<span className="bg-white px-6 text-gray-900">
+										{loginDetails.loginSpan}
+										<button className="underline" onClick={handleToggle}>
+											{loginDetails.loginLink}
+										</button>
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="hidden w-0 flex-1 py-12 lg:block">
+						<img
+							className="h-full w-full rounded-3xl object-cover"
+							src={loginDetails.image}
+							alt="login image"
+						/>
+					</div>
+				</div>
+			</Container>
+		</Layout>
+	)
 }
 
+export default LoginPage
--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,42 +1,40 @@
-import Head from "next/head";
-import Container from "../components/container";
-import Layout from "../components/layout";
-import { createClient } from "../lib/prismic";
-import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
-import PageBody from "components/page-body";
+import PageBody from 'components/page-body'
+import { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import Head from 'next/head'
+
+import Container from '../components/container'
+import Layout from '../components/layout'
+import { createClient } from '../lib/prismic'
 
 type IndexProps = InferGetStaticPropsType<typeof getStaticProps>
 
-export default function TermsPage({ preview, page}: IndexProps) {
-
-  return (
-    <>
-      <Layout preview={preview} >
-        <Head>
-          <title>Beyond Health</title>
-        </Head>
-        <div className="bg-gray-000 min-h-screen sm:py-10">
-          <Container>
-            <div className="max-w-[1120px] mx-auto">
-              {/* @ts-ignore */}
-              <PageBody slices={page.data.slices} />
-            </div>
-          </Container>
-        </div>
-      </Layout>
-    </>
-  );
+const TermsPage = ({ preview, page }: IndexProps) => {
+	return (
+		<>
+			<Layout preview={preview}>
+				<Head>
+					<title>Beyond Health</title>
+				</Head>
+				<div className="min-h-screen bg-gray-000 sm:py-10">
+					<Container>
+						<div className="mx-auto max-w-[1120px]">
+							<PageBody slices={page.data.slices} />
+						</div>
+					</Container>
+				</div>
+			</Layout>
+		</>
+	)
 }
 
-export async function getStaticProps({
-  preview = false,
-  previewData,
-}: GetStaticPropsContext) {
-  const client = createClient({ previewData });
-  /* @ts-ignore */
-  const page = await client.getSingle("privacy_policy");
+export async function getStaticProps({ preview = false, previewData }: GetStaticPropsContext) {
+	const client = createClient({ previewData })
 
-  return {
-    props: { preview, page },
-  };
+	const page = await client.getSingle('privacy_policy')
+
+	return {
+		props: { preview, page }
+	}
 }
+
+export default TermsPage
