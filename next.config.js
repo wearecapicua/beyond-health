fad5001ecad5dda7b04a61bd99832c1102d3d19b
Alexandrei <alexandreirdp@gmail.com>
2024-01-31 20:35:45 -0400

    return

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	// images: {
-	// 	domains: [
-	// 		'images.prismic.io',
-	// 		'files.stripe.com',
-	// 		'lh3.googleusercontent.com',
-	// 		'platform-lookaside.fbsbx.com'
-	// 	]
-	// },
 	images: {
-		remotePatterns: [
-		{
-		   protocol: "https",
-		   hostname: "**",
-		 },
-		],
-	 },
+		domains: [
+			'images.prismic.io',
+			'files.stripe.com',
+			'lh3.googleusercontent.com',
+			'platform-lookaside.fbsbx.com'
+		]
+	},
 	webpack5: true,
 	webpack: (config) => {
 		config.resolve.fallback = { fs: false }
