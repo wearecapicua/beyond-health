99a3e6ed307fb3f4638b7ce6b6b21e83133b0ce2
Alexandrei <alexandreirdp@gmail.com>
2024-01-31 20:21:20 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+	// images: {
+	// 	domains: [
+	// 		'images.prismic.io',
+	// 		'files.stripe.com',
+	// 		'lh3.googleusercontent.com',
+	// 		'platform-lookaside.fbsbx.com'
+	// 	]
+	// },
 	images: {
-		domains: [
-			'images.prismic.io',
-			'files.stripe.com',
-			'lh3.googleusercontent.com',
-			'platform-lookaside.fbsbx.com/platform/profilepic'
-		]
-	},
+		remotePatterns: [
+		{
+		   protocol: "https",
+		   hostname: "**",
+		 },
+		],
+	 },
 	webpack5: true,
 	webpack: (config) => {
 		config.resolve.fallback = { fs: false }
