3927fad31fc5685d163ebed5794cb7e6d77a6c6d
Ariel Prados <ariel@capicua.com.uy>
2025-09-02 18:30:28 -0300

    Fixed typo

--- pages/api/nuvei/generateChecksum.ts
+++ pages/api/nuvei/generateChecksum.ts
@@ -1,16 +1,16 @@
-import crypto from 'crypto';
+import crypto from 'crypto'
 
-import env from 'lib/env';
-import { NextApiRequest, NextApiResponse } from 'next';
+import env from 'lib/env'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 const generateChecksum = (req: NextApiRequest, res: NextApiResponse) => {
-    const { publicMerchantId, publicMerchantSiteId, productPrice, timeStamp } = req.body;
-    const clientRequestId = ""
-    const checksumData = `${publicMerchantId}${publicMerchantSiteId}${clientRequestId}${productPrice}USD${timeStamp}${env.merchantSecretKey}`;
-    
-    const checksum = crypto.createHash('sha256').update(checksumData, 'utf8').digest('hex');
+	const { publicMerchantId, publicMerchantSiteId, productPrice, timeStamp } = req.body
+	const clientRequestId = ''
+	const checksumData = `${publicMerchantId}${publicMerchantSiteId}${clientRequestId}${productPrice}USD${timeStamp}${env.publicMerchantSecretKey}`
 
-    res.status(200).json({ checksum });
-};
+	const checksum = crypto.createHash('sha256').update(checksumData, 'utf8').digest('hex')
 
-export default generateChecksum;
+	res.status(200).json({ checksum })
+}
+
+export default generateChecksum
